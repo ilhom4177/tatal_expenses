842572df718a67adc7f1630a7830ccd9520f50b1
Jalilov <javohirjalilov777@gmail.com>
2022-12-22 18:42:44 +0500

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+# Living Total Expenses
---
+++ main.py
@@ -0,0 +1,10 @@
+# Calculate the total expenses
+def total_expenses(file_name: str) -> int:
+    """
+    Calculate the total expenses
+    Args:
+        file_name: str
+    Returns:
+        total_expenses: total expenses
+    """
+    return 
